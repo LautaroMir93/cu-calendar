4655a1ec4aee8b2a9e215682a55c77d890b2cf85
Lautaro Mirenda <lautaromir93@gmail.com>
2019-02-15 23:49:49 -0300

    Changed app path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '@modules/app/containers/App';
 import { Provider } from 'react-redux'
 import configureStore from './store';
 import * as serviceWorker from './serviceWorker';
